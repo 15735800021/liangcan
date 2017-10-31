a7775d434b44c7a72bc7c25b5b8efaa0d3e96d0a
15735800021 <1031091715@qq.com>
2017-10-31 20:48:22 +0800

    第二天任务完成

--- 郝--良仓/shouye.js
+++ 郝--良仓/shouye.js
@@ -32,7 +32,7 @@ $(".n").mouseleave(function(){
 $("footer p a").mouseover(function(){
     $("footer p a i").animate({"left":320},500);
 })
-$("footer p").mouseover(function(){
+$("footer p a").mouseover(function(){
     $("footer p a i").animate({"left":350},500);
 })
 $(window).scroll(function(){
@@ -43,3 +43,77 @@ $(window).scroll(function(){
         $(".top").hide();
     }
 });
+// 轮播图
+var w = $("#m_unit ul li").innerWidth();
+$("#rightBtn").click(function() {
+	move();
+});
+$("#leftBtn").click(function() {
+    if ($("#m_unit").is(":animated")) {
+		return ; 
+	}
+    i--;
+    if (i < 0) {
+			i = 4;  
+            $("#m_unit").css({"left":5000}); 
+		}
+	$("#m_unit").animate({"left": -i*w}, 1000, function() {
+		
+		$(".circles ol li").removeClass("current");
+		$(".circles ol li").eq(i).addClass("current");
+    });	
+    
+});
+$('#carousel').hover(function() {
+    clearInterval(move);
+});
+setInterval(move, 3000);
+var i = 0;
+  function move() {
+	if ($("#m_unit").is(":animated")) {
+		return ; 
+	}
+	i++;
+	$("#m_unit").animate({"left": -i*w}, 1000, function() {
+		if (i >= 5) {
+			i = 0;  
+            $("#m_unit").css({"left": 0}); 
+		}
+		$(".circles ol li").removeClass("current");
+		$(".circles ol li").eq(i).addClass("current");
+	});	
+}
+//请求后台商品
+var url = "http://h6.duchengjiu.top/shop/api_goods.php";
+var data = {
+    "cat_id":"0",
+    // "page":"0",
+    // "pagesize":"0"
+}
+var a=[];
+$.ajax({
+    url:url,
+    type:"GET",
+    data:data,
+    success:function(str){
+        var a=str.data;
+        for(var i=0;i<a.length-1;i++){
+            console.log(str);
+        $(".shoppings ul").append("<a href=''><li></li></a>");
+        $(".shoppings ul li").eq(i).css("background-image","url("+a[i].goods_thumb+")");
+        if(i%3==0){
+            $(".shoppings ul li").eq(i).css("margin-left","0");
+        }
+        } 
+        $(".shoppings ul li").append("<div></div>");
+        $(".shoppings ul li").each(function(i){
+            $(this).children("div").append("<i>"+"￥"+a[i].price+"</i>");
+        })
+        $(".shoppings ul li").each(function(i){
+            $(this).children("div").append("<h3>"+a[i].goods_name+"</h3>");
+        })
+        $(".shoppings ul li").each(function(i){
+            $(this).children("div").append("<p>"+a[i].goods_desc+"</p>");
+        })
+    }
+})
